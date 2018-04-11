1b7c949d2336e276d675ed111cc1a890a6fcb995
tgroc <tgroc@172.19.26.168>
2018-04-12 01:19:07 +0700

    Master commit

--- login/src/OOP/wallet.java
+++ login/src/OOP/wallet.java
@@ -1,9 +1,44 @@
 package OOP;
 
+import java.util.*;
+
 public abstract class wallet {
+    
+    public int p = 0;
          
     public abstract void point();
     
+    public void print(){
     
+        System.out.println("Your point is "+p+" Point" );
         
+    }       
+}
+
+class Point extends wallet{
+    
+    
+    
+    public void print(){
+    
+        Scanner yn = new Scanner(System.in);
+        System.out.print("Do you want to fill point (Y/N) >> ");
+        String y = yn.nextLine();
+        char n = y.charAt(0);
+        
+        switch(n){
+            case 'y': point();break;
+            case 'n': break;
+            default : print();break;
+        
+        }    
+    }
+    
+    public void point(){
+        
+      
+    
+    
+    } 
+
 }
